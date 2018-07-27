e6a3dd708a6a6830f28a667626fd6d450d1590a3
Cierra Washington <cierrajwashi@gmail.com>
2018-07-27 12:54:06 -0600

    Add initial canvas and bars for bar graph

---
+++ script.js
@@ -0,0 +1,45 @@
+var canvas = document.querySelector('#bargraph');
+
+var ctx = canvas.getContext('2d');
+
+// var X = 0;
+// var Y = 0;
+// var width = 500;
+// var height = 50;
+
+ctx.fillStyle = "red";
+
+// ctx.fillRect(100, 100, 20, 300);
+ctx.fillRect(10, 50, 15, 300);
+ctx.fillRect(40, 50, 15, 300);
+ctx.fillRect(70, 50, 15, 300);
+ctx.fillRect(100, 50, 15, 300);
+ctx.fillRect(130, 50, 15, 300);
+ctx.fillRect(160, 50, 15, 300);
+ctx.fillRect(190, 150, 15, 300);
+ctx.fillRect(220, 150, 15, 300);
+ctx.fillRect(250, 150, 15, 300);
+ctx.fillRect(280, 150, 15, 300);
+
+
+console.log(canvas);
+
+
+// function draw(){
+//       // var canvas = $('#bargraph');
+
+//       var canvas = document.querySelector('#bargraph');
+//       var context = canvas.getContext("2d");
+
+//       var X = 0;
+//       var Y = 0;
+//       var width = 500;
+//       var height = 50;
+
+//       context.fillStyle = "red";
+
+//       context.fillRect(X, Y, width, height);
+
+//       console.log()
+//     }
+
